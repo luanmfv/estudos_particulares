52947a09308e58f827a0924139bc953bb0ef8c0b
Luan <luanmiguel.contato@outlook.com>
2025-07-08 14:05:41 -0300

    changes

--- estudos_particulares/tests/test_simples_test.py
+++ estudos_particulares/tests/test_simples_test.py
@@ -1,7 +1,14 @@
 def my_function(x: int) -> int:
+    if not isinstance(x, int):
+        raise ValueError("x must be of int type")
     return x * 2
 
 def test_my_function_returns_double_the_input():
     actual_result = my_function(x=5)
 
     assert actual_result == 10
+
+
+def test_my_function_raises_when_x_is_not_int():
+    with pytest.raises(ValueError):
+        my_function(x=1.345435)
